1a479731252c4f2e0cb4feb5b9f8c5cf3dc7a1e1
Deepak Yadav <Deepak.y@byosync.in>
2025-11-17 16:59:36 +0530

    fix: Correct BASE_URL configuration for production environment

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export const env = {
-    BASE_URL: "https://byo-sync-backend-testing.vercel.app/api/v1",
-    // BASE_URL: "https://backendapi.byosync.in/api/v1",
+    // BASE_URL: "https://byo-sync-backend-testing.vercel.app/api/v1",
+    BASE_URL: "https://backendapi.byosync.in/api/v1",
     // BASE_URL: "http://localhost:7000/api/v1",
 
 }
